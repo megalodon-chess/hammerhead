3b47cf42f2f5b35dd5696cd325a35ad592c8f83b
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-03-17 10:59:44 -0700

    Made fonts larger.

--- src/constants.py
+++ src/constants.py
@@ -27,9 +27,9 @@ IMG_DIR = os.path.join(PARENT, "images")
 FPS = 60
 IMAGES = {}
 
-FONT_SMALL = pygame.font.SysFont("arial", 12)
-FONT_MED = pygame.font.SysFont("arial", 20)
-FONT_LARGE = pygame.font.SysFont("arial", 32)
+FONT_SMALL = pygame.font.SysFont("arial", 14)
+FONT_MED = pygame.font.SysFont("arial", 24)
+FONT_LARGE = pygame.font.SysFont("arial", 36)
 
 BLACK = (0, 0, 0)
 GRAY_DARK = (64, 64, 64)
