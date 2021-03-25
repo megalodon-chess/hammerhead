0fdc84fc8d7a2ca7ca1a1fe3a66d5abafa3134fa
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-03-25 08:25:27 -0700

    Moved analysis to another file.

---
+++ src/analyze.py
@@ -0,0 +1,130 @@
+#
+#  Hammerhead
+#  Chess GUI written in Python.
+#  Copyright Megalodon Chess 2021
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+import os
+import threading
+import pygame
+import chess
+import chess.engine
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+from constants import *
+from elements import Button, centered_text
+from board import Board
+pygame.init()
+Tk().withdraw()
+
+
+class Analysis:
+    def __init__(self):
+        self.session = 0
+        self.eng = ""
+        self.info = {}
+        self.in_progress = False
+        self.load_eng = Button()
+        self.start = Button()
+        self.stop = Button()
+
+    def nodes_text(self, num):
+        if num < 10**5:
+            suffix = ""
+        elif 10**5 <= num < 10**8:
+            suffix = "k"
+            num = int(num/10**3)
+        elif 10**8 <= num:
+            suffix = "m"
+            num = int(num/10**6)
+        string = suffix
+        count = 0
+        for digit in reversed(str(num)):
+            string = digit + string
+            if count % 3 == 2:
+                string = "," + string
+            count += 1
+        if string.startswith(","):
+            string = string[1:]
+        return string
+
+    def analyze(self, eng_path, wm):
+        self.info = {"depth": 0, "nodes": 0, "nps": 0, "score": None, "time": 0}
+        self.in_progress = True
+
+        start_move_num = wm.board.move_num
+        session = self.session
+        engine = chess.engine.SimpleEngine.popen_uci(eng_path)
+        if "Threads" in engine.options:
+            engine.configure({"Threads": CORES})
+        restart = False
+        with engine.analysis(wm.board.view_board) as analysis:
+            keys = ("depth", "nodes", "nps", "time")
+            for info in analysis:
+                for key in keys:
+                    if key in info:
+                        self.info[key] = info[key]
+                if "score" in info:
+                    self.info["score"] = int(str(info["score"].pov(chess.WHITE))) / 100
+
+                if not wm.active or self.session != session:
+                    restart = False
+                    break
+                if wm.board.move_num != start_move_num:
+                    restart = True
+                    break
+
+        engine.quit()
+        self.in_progress = False
+        if restart:
+            threading.Thread(target=self.analyze, args=(eng_path,)).start()
+
+    def draw(self, surface, events, loc, size, wm):
+        self.load_eng.draw(surface, events, (loc[0]+25, loc[1]+25), (150, 35), "Load Engine")
+        self.start.draw(surface, events, (loc[0]+25, loc[1]+70), (150, 35), "Start Analysis")
+        self.stop.draw(surface, events, (loc[0]+25, loc[1]+115), (150, 35), "Stop Analysis")
+        centered_text(surface, (loc[0]+200, loc[1]+42), BLACK, FONT_SMALL, self.eng, cx=False)
+
+        if self.load_eng.clicked(events) and not self.in_progress:
+            settings = load_settings()
+            kwargs = {"initialdir": settings["eng_path"]} if "eng_path" in settings else {}
+            path = askopenfilename(title="Select Engine", **kwargs)
+            if isinstance(path, str) and os.path.isfile(path):
+                path = os.path.realpath(path)
+                self.eng = path
+                settings["eng_path"] = os.path.dirname(path)
+                save_settings(settings)
+        if self.start.clicked(events) and not self.in_progress and os.path.isfile(self.eng):
+            threading.Thread(target=self.analyze, args=(self.eng, wm)).start()
+        if self.stop.clicked(events) and self.in_progress:
+            self.session += 1
+
+        text = "Analysis in progress" if self.in_progress else "Analysis not in progress"
+        centered_text(surface, (loc[0]+25, loc[1]+225), BLACK, FONT_MED, text, cx=False)
+        info = (
+            ("Depth", "depth"),
+            ("Nodes", "nodes"),
+            ("Nodes per second", "nps"),
+            ("Time", "time"),
+            ("Score", "score"),
+        )
+        for i, (text, key) in enumerate(info):
+            if key in self.info:
+                key_info = self.info[key]
+                text = text + ": "
+                text += self.nodes_text(key_info) if "Nodes" in text else str(key_info)
+                y = loc[1] + 275 + 25*i
+                centered_text(surface, (loc[0]+25, y), BLACK, FONT_SMALL, text, cx=False)
--- src/wm.py
+++ src/wm.py
@@ -17,18 +17,12 @@
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-import os
-import threading
 import pygame
-import chess
-import chess.engine
-from tkinter import Tk
-from tkinter.filedialog import askopenfilename
 from constants import *
 from elements import Button, centered_text
 from board import Board
+from analyze import Analysis
 pygame.init()
-Tk().withdraw()
 
 
 class WindowManager:
@@ -37,66 +31,8 @@ class WindowManager:
         self.menus = ("Game", "Analysis", "Engines")
         self.tab = 0
 
-        self.analysis_session = 0
-        self.analysis_eng = ""
-        self.analysis_info = {}
-        self.analysis_in_progress = False
-        self.analysis_load_eng = Button()
-        self.analysis_start = Button()
-        self.analysis_stop = Button()
-
         self.board = Board()
-
-    def nodes_text(self, num):
-        if num < 10**5:
-            suffix = ""
-        elif 10**5 <= num < 10**8:
-            suffix = "k"
-            num = int(num/10**3)
-        elif 10**8 <= num:
-            suffix = "m"
-            num = int(num/10**6)
-        string = suffix
-        count = 0
-        for digit in reversed(str(num)):
-            string = digit + string
-            if count % 3 == 2:
-                string = "," + string
-            count += 1
-        if string.startswith(","):
-            string = string[1:]
-        return string
-
-    def analyze(self, eng_path):
-        self.analysis_info = {"depth": 0, "nodes": 0, "nps": 0, "score": None, "time": 0}
-        self.analysis_in_progress = True
-
-        start_move_num = self.board.move_num
-        session = self.analysis_session
-        engine = chess.engine.SimpleEngine.popen_uci(eng_path)
-        if "Threads" in engine.options:
-            engine.configure({"Threads": CORES})
-        restart = False
-        with engine.analysis(self.board.view_board) as analysis:
-            keys = ("depth", "nodes", "nps", "time")
-            for info in analysis:
-                for key in keys:
-                    if key in info:
-                        self.analysis_info[key] = info[key]
-                if "score" in info:
-                    self.analysis_info["score"] = int(str(info["score"].pov(chess.WHITE))) / 100
-
-                if not self.active or self.analysis_session != session:
-                    restart = False
-                    break
-                if self.board.move_num != start_move_num:
-                    restart = True
-                    break
-
-        engine.quit()
-        self.analysis_in_progress = False
-        if restart:
-            threading.Thread(target=self.analyze, args=(eng_path,)).start()
+        self.analysis = Analysis()
 
     def draw(self, surface, events, force_redraw=False):
         # Split surface into sections
@@ -135,38 +71,4 @@ class WindowManager:
         pygame.draw.rect(surface, TAB_SEL, (menu_loc[0], menu_loc[1]+tab_height, menu_size[0], menu_size[1]-tab_height))
 
         if self.menus[self.tab] == "Analysis":
-            self.analysis_load_eng.draw(surface, events, (menu_loc[0]+25, menu_loc[1]+tab_height+25), (150, 35), "Load Engine")
-            self.analysis_start.draw(surface, events, (menu_loc[0]+25, menu_loc[1]+tab_height+70), (150, 35), "Start Analysis")
-            self.analysis_stop.draw(surface, events, (menu_loc[0]+25, menu_loc[1]+tab_height+115), (150, 35), "Stop Analysis")
-            centered_text(surface, (menu_loc[0]+200, menu_loc[1]+tab_height+42), BLACK, FONT_SMALL, self.analysis_eng, cx=False)
-
-            if self.analysis_load_eng.clicked(events) and not self.analysis_in_progress:
-                settings = load_settings()
-                kwargs = {"initialdir": settings["analysis_eng_path"]} if "analysis_eng_path" in settings else {}
-                path = askopenfilename(title="Select Engine", **kwargs)
-                if isinstance(path, str) and os.path.isfile(path):
-                    path = os.path.realpath(path)
-                    self.analysis_eng = path
-                    settings["analysis_eng_path"] = os.path.dirname(path)
-                    save_settings(settings)
-            if self.analysis_start.clicked(events) and not self.analysis_in_progress and os.path.isfile(self.analysis_eng):
-                threading.Thread(target=self.analyze, args=(self.analysis_eng,)).start()
-            if self.analysis_stop.clicked(events) and self.analysis_in_progress:
-                self.analysis_session += 1
-
-            text = "Analysis in progress" if self.analysis_in_progress else "Analysis not in progress"
-            centered_text(surface, (menu_loc[0]+25, menu_loc[1]+225), BLACK, FONT_MED, text, cx=False)
-            info = (
-                ("Depth", "depth"),
-                ("Nodes", "nodes"),
-                ("Nodes per second", "nps"),
-                ("Time", "time"),
-                ("Score", "score"),
-            )
-            for i, (text, key) in enumerate(info):
-                if key in self.analysis_info:
-                    key_info = self.analysis_info[key]
-                    text = text + ": "
-                    text += self.nodes_text(key_info) if "Nodes" in text else str(key_info)
-                    y = menu_loc[1] + 275 + 25*i
-                    centered_text(surface, (menu_loc[0]+25, y), BLACK, FONT_SMALL, text, cx=False)
+            self.analysis.draw(surface, events, (menu_loc[0]+25, menu_loc[1]+25+tab_height), menu_size, self)
